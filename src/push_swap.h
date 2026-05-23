29112dfcf55ecaff423b7725c6122eca3472eada
Filipa Dos Reis Gregorio <fdos-rei@c1r4s1.42lisboa.com>
2026-05-23 18:34:54 +0100

    added the main push_swap and parsing folder with initial thoughts and structure

---
+++ src/push_swap.h
@@ -0,0 +1,11 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h> // para write & read
+#include <stdlib.h> // para malloc & free
+#include "libft.h"
+
+//declaracao de typedefs para linked lists
+//declaracao de funcoes
+
+#endif
