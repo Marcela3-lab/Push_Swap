5384cc10a2d7b4bb8644a2f222f2049e1c66105b
Filipa Dos Reis Gregorio <fdos-rei@c1r5s2.42lisboa.com>
2026-06-01 15:42:01 +0100

    added complex algorithm

---
+++ src/sorting/complex.c
@@ -0,0 +1,55 @@
+#include "../push_swap.h"
+
+static unsigned int	count_bits(int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (n != 0)
+	{
+		n = n / 2;
+		i++;
+	}
+	return (i);
+}
+void	complex(t_data *data)
+{
+	int	i;
+	int	n;
+	int	j;
+	t_node	*node;
+
+	i = 0;
+	j = data->stack_a->size - 1;
+	n = count_bits(j);
+	while (i < n)
+	{
+		j = data->stack_a->size - 1;
+		node = data->stack_a->head;
+		while (j >= 0)
+		{
+			if((node->index >> i) & 1)
+				ra(data);
+			else
+				pb(data);
+			node = data->stack_a->head;
+			j--;
+		}
+		while (data->stack_b->size != 0)
+			pa(data);
+		i++;
+	}
+
+}
+
+
+/* //test count_bits
+int main ()
+{
+	int	n = 1;
+	int n1 = 10;
+	int n2 = 10045851;
+
+	printf("%u\n%u\n%u\n", count_bits(n), count_bits(n1), count_bits(n2));
+	return(0);
+} */
